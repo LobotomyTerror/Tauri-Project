8063d17955e868987edc0860e8a29a2b0c1e2b0f
LobotomyTerror <dfishbein90@gmail.com>
2025-12-27 00:39:43 -0700

    initial commit

---
+++ Sample Project/src-tauri/gen/android/app/src/main/java/com/danielfishbein/sample_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.danielfishbein.sample_project
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
